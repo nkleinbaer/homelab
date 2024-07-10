9ea6509dc6ab1eae8b26274f601cc1a280572eac
Nicholas Klein-Baer <nicholaskleinbaer@gmail.com>
2024-07-10 13:48:28 -0600

    update mdns

--- mdns/publish-mdns-addresses.sh
+++ mdns/publish-mdns-addresses.sh
@@ -7,5 +7,6 @@ function _term {
 trap _term SIGTERM
 
 /usr/bin/avahi-publish -a grafana.local -R 192.168.1.243 &
+/usr/bin/avahi-publish -a plane.local -R 192.168.1.243 &
 
 while true; do sleep 10000; done
