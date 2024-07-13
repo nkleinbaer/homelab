101aa6c4636f2f5607f296f6ad45b5f17025f610
Nicholas Klein-Baer <nicholaskleinbaer@gmail.com>
2024-07-13 15:44:46 -0600

    Add stac-fastapi-pgstac

--- mdns/publish-mdns-addresses.sh
+++ mdns/publish-mdns-addresses.sh
@@ -9,5 +9,6 @@ trap _term SIGTERM
 /usr/bin/avahi-publish -a grafana.local -R 192.168.1.243 &
 /usr/bin/avahi-publish -a plane.local -R 192.168.1.243 &
 /usr/bin/avahi-publish -a plane-minio.local -R 192.168.1.243 &
+/usr/bin/avahi-publish -a stac.local -R 192.168.1.243 &
 
 while true; do sleep 10000; done
